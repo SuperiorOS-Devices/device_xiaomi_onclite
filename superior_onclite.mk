5461a4b014801c7d238aa16992dc9542c1a0cc00
abhayksanjay <abhayk8423@gmail.com>
2021-09-29 22:30:33 +0530

    onclite: Update redfin fp to September

--- superior_onclite.mk
+++ superior_onclite.mk
@@ -24,9 +24,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc_eea-user 10 QKQ1.191008.001/V11.0.1.0.QFLEUXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
