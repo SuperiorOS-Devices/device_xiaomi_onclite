276766f9c81e3722211ba557f5c7a033aa134ad9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-17 09:51:07 +0530

    onclite: Build all extra VNDK versions * Basically make system a GSI, but this should avoid us the headache made by VNDK libs not being built based on proprietary interfaces

--- device.mk
+++ device.mk
@@ -408,6 +408,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.msm8953
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
